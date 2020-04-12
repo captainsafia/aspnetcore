1d590245f0552649f0947dc55b5841ba15454801
Safia Abdalla <safia@safia.rocks>
2020-04-11 19:46:39 -0700

    Commit

--- src/ProjectTemplates/ComponentsWebAssembly.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/Program.cs
+++ src/ProjectTemplates/ComponentsWebAssembly.ProjectTemplates/content/ComponentsWebAssembly-CSharp/Client/Program.cs
@@ -30,18 +30,19 @@ namespace ComponentsWebAssembly_CSharp
                 // Configure your authentication provider options here.
                 // For more information, see https://aka.ms/blazor-standalone-auth
                 #endif
-                options.ProviderOptions.Authority = "https://login.microsoftonline.com/";
-                options.ProviderOptions.ClientId = "33333333-3333-3333-33333333333333333";
+                builder.Configuration.Bind("Authority", options.ProviderOptions.Authority);
+                builder.Configuration.Bind("ClientId", options.ProviderOptions.ClientId);
             });
     #endif
 #endif
 #if (IndividualB2CAuth)
             builder.Services.AddMsalAuthentication(options =>
             {
+
                 var authentication = options.ProviderOptions.Authentication;
-                authentication.Authority = "https:////aadB2CInstance.b2clogin.com/qualified.domain.name/MySignUpSignInPolicyId";
-                authentication.ClientId = "33333333-3333-3333-33333333333333333";
-                authentication.ValidateAuthority = false;
+                builder.Configuration.Bind("Authority", authentication.Authority);
+                builder.Configuration.Bind("ClientId", authentication.ClientId);
+                builder.Configuration.Bind("ValidateAuthority", authentication.ValidateAuthority)
 #if (Hosted)
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("https://qualified.domain.name/api.id.uri/api-scope");
 #endif
@@ -51,8 +52,8 @@ namespace ComponentsWebAssembly_CSharp
             builder.Services.AddMsalAuthentication(options =>
             {
                 var authentication = options.ProviderOptions.Authentication;
-                authentication.Authority = "https://login.microsoftonline.com/22222222-2222-2222-2222-222222222222";
-                authentication.ClientId = "33333333-3333-3333-33333333333333333";
+                builder.Configuration.Bind("Authority", authentication.Authority);
+                builder.Configuration.Bind("ClientId", authentiation.ClientId);
 #if (Hosted)
                 options.ProviderOptions.DefaultAccessTokenScopes.Add("api://api.id.uri/api-scope");
 #endif
