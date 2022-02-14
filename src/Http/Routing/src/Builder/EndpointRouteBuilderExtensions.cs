5f6d1c527c24527244ab1d36d0bb2e44df240ebb
Safia Abdalla <safia@safia.rocks>
2022-02-14 16:52:24 +0000

    Add support for endpoint filters in minimal APIs

--- src/Http/Routing/src/Builder/EndpointRouteBuilderExtensions.cs
+++ src/Http/Routing/src/Builder/EndpointRouteBuilderExtensions.cs
@@ -487,10 +487,7 @@ public static class EndpointRouteBuilderExtensions
             DisableInferBodyFromParameters = disableInferBodyFromParameters,
         };
 
-        var requestDelegateResult = RequestDelegateFactory.Create(handler, options);
-
         var builder = new RouteEndpointBuilder(
-            requestDelegateResult.RequestDelegate,
             pattern,
             defaultOrder)
         {
@@ -516,12 +513,6 @@ public static class EndpointRouteBuilderExtensions
         // Add delegate attributes as metadata
         var attributes = handler.Method.GetCustomAttributes();
 
-        // Add add request delegate metadata
-        foreach (var metadata in requestDelegateResult.EndpointMetadata)
-        {
-            builder.Metadata.Add(metadata);
-        }
-
         // This can be null if the delegate is a dynamic method or compiled from an expression tree
         if (attributes is not null)
         {
@@ -538,6 +529,18 @@ public static class EndpointRouteBuilderExtensions
             endpoints.DataSources.Add(dataSource);
         }
 
-        return new RouteHandlerBuilder(dataSource.AddEndpointBuilder(builder));
+        var routeHandlerBuilder = new RouteHandlerBuilder(dataSource.AddEndpointBuilder(builder));
+        routeHandlerBuilder.Add(endpointBuilder =>
+        {
+            var filteredRequestDelegateResult = RequestDelegateFactory.Create(handler, options, routeHandlerBuilder.RouteHandlerFilters);
+            // Add add request delegate metadata
+            foreach (var metadata in filteredRequestDelegateResult.EndpointMetadata)
+            {
+                endpointBuilder.Metadata.Add(metadata);
+            }
+            endpointBuilder.RequestDelegate = filteredRequestDelegateResult.RequestDelegate;
+        });
+
+        return routeHandlerBuilder;
     }
 }
